d17b48ccaa4df6d7dd7c39667dd814b6eef96639
Rev3lation <kaisern.koh@gmail.com>
2025-08-26 09:22:37 +0800

    Implements Moves.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,7 @@ import Items from './components/Items.vue'
 import Wild from './components/Wild.vue'
 import DVs from './components/DVs.vue'
 import EVs from './components/EVs.vue'
+import Moves from './components/Moves.vue'
 import Happiness from './components/Happiness.vue'
 import Money from './components/Money.vue'
 import AbilityNatureShiny from './components/AbilityNatureShiny.vue'
@@ -11,7 +12,7 @@ import Select from 'primevue/select'
 import Divider from 'primevue/divider';
 
 const selectedCheat = ref(null)
-const cheats = ["Item Modifier", "Wild Pokemon Modifier", "DV Modifier", "EV Modifier", "Ability / Nature / Shininess Modifier", "Happiness Modifier", "Money Modifier"];
+const cheats = ["Item Modifier", "Wild Pokemon Modifier", "DV Modifier", "EV Modifier", "Moveset Modifier", "Ability / Nature / Shininess Modifier", "Happiness Modifier", "Money Modifier"];
 
 </script>
 
@@ -41,6 +42,7 @@ const cheats = ["Item Modifier", "Wild Pokemon Modifier", "DV Modifier", "EV Mod
   <Items class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='Item Modifier'"/>
   <DVs class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='DV Modifier'"/>
   <EVs class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='EV Modifier'"/>
+  <Moves class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='Moveset Modifier'"/>
   <AbilityNatureShiny class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='Ability / Nature / Shininess Modifier'"/>
   <Happiness class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='Happiness Modifier'"/>
   <Money class="mt-7 ml-5 mr-5 mb-100" v-if="selectedCheat==='Money Modifier'"/>
---
+++ frontend/src/components/Moves.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import Card from 'primevue/card'
+import Select from 'primevue/select'
+import { addressExtend } from '../lib/addressExtend.js'
+
+
+const loaded = ref(false)
+const addresses = ref(null)
+const moves = ref(null)
+const selectedMoves = ref([null, null, null, null])
+
+const fetchAddresses = async () => {
+  let responseAddresses = await fetch("https://polishedcheats-backend.vercel.app/api/addresses") 
+  let responseMoves = await fetch("https://polishedcheats-backend.vercel.app/api/moves")
+  addresses.value = await responseAddresses.json()
+  moves.value = await responseMoves.json()
+}
+
+onMounted(() => {
+  fetchAddresses();
+  loaded.value = true;
+});
+
+const getMoveCode = (selectedMoves) => {
+  //Retrieve the right address
+  let addressList = addressExtend(addresses.value["wPartyMon1Moves"], 4);
+  //Moves consists of four consecutive addresses, one for each move
+  let cheatValues = selectedMoves.map((move) => {
+    move = (moves.value.indexOf(move) + 1).toString(16);
+    move = "0".repeat(2 - move.length) + move;
+    return move;
+  });
+
+  return ("01" + cheatValues[0] + addressList[0] + " 01" + cheatValues[1] + addressList[1] + " 01" + cheatValues[2] + addressList[2] + " 01" + cheatValues[3] + addressList[3]).toUpperCase();
+}
+
+</script>
+
+<template>
+  <Card>
+    <template #title>Moves</template>
+    <template #content>
+      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-2 mb-1">
+        <Select class="mt-2 mb-5" v-if="loaded" v-model="selectedMoves[0]" :options="moves" filter placeholder="Select Move 1"/>
+        <Select class="mt-2 mb-5" v-if="loaded" v-model="selectedMoves[1]" :options="moves" filter placeholder="Select Move 2"/>
+        <Select class="mt-2 mb-5" v-if="loaded" v-model="selectedMoves[2]" :options="moves" filter placeholder="Select Move 3"/>
+        <Select class="mt-2 mb-5" v-if="loaded" v-model="selectedMoves[3]" :options="moves" filter placeholder="Select Move 4"/>
+      </div>
+      <p class="mb-5" v-if="selectedMoves[0] && selectedMoves[1] && selectedMoves[2] && selectedMoves[3]">Your code for this moveset is: {{ getMoveCode(selectedMoves) }}</p>
+      <p class="mb-5" v-else>Please choose four moves.</p>
+      <p>This code modifies the moveset of the first Pokemon in your party. <br>
+        Note that this allows the Pokemon to learn moves it typically cannot. <br>
+        Should you wish to change the moveset of the other Pokemon in your party, simply swap the party order to place them first.
+      </p>
+    </template>
+  </Card>
+</template>
+
+<style scoped></style>
